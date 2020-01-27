c10eddcea3ce898a3c9f5d84574c96400ed2991e
Tomasz Przytula <t.przytula.dev@gmail.com>
2020-01-27 21:24:41 +0000

    Connect to NATS in backend, create sample subscriptions, publish sample events

--- services/backend/index.js
+++ services/backend/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 
+const NATS = require('nats')
+const nc = NATS.connect({
+    url: 'nats://nats:4222',
+    json: true
+});
+ 
+nc.subscribe('foo', message => {
+  console.log('Received a message: ', message);
+});
+
+nc.subscribe('user-connections', message => {
+  console.log('New user request:', message);
+});
+
+nc.publish('foo', { message: 'Hello World!' })
+
 app.get('/', (req, res) => {
+    nc.publish('user-connections', {
+        ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        query: req.query
+    });
+
     res.send('Hello there!');
 });
 
